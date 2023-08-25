07d9a2dc8fc39b0dc15093665ad33980d9c321ca
Damien Aicheh <damienaicheh@microsoft.com>
2023-08-25 17:39:39 +0200

    feat: Start Terraform scripts.

---
+++ terraform/app.tf
@@ -0,0 +1,11 @@
+resource "azurerm_linux_web_app" "this" {
+  name                = format("app-%s", local.resource_suffix_kebabcase)
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_service_plan.this.location
+  service_plan_id     = azurerm_service_plan.this.id
+
+  tags = local.tags
+
+  site_config {
+  }
+}
