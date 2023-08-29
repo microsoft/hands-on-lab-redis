863117d1aa44949c37c9e155682ca4ecef3157f3
Julien Strebler <durieljulien@gmail.com>
2023-08-29 11:29:18 +0200

    feat + refactor : Pattern Products Event trigger AzFunc can now be triggered based on a key pattern *_Products:all Refactored code structure/namespaces Cleaned VSCode debug settings. Added : APIM Cache-value-store policy

---
+++ src/HoL.Redis.Func.Watcher/ProductsCacheRefresh.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.ComponentModel;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Redis;
+using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
+
+namespace HoL.Redis.Func.Watcher
+{
+    public static class RedisCacheRefresh
+    {
+        [Description("This function will be triggered when the DEL command is being executed")]
+        [FunctionName("ProductsEvents")]
+        public static void ProductsEventsTrigger(
+            [RedisPubSubTrigger("REDIS_CONNECTION_STRING", "__keyspace@0__:*_%REDIS_PRODUCT_ALL%")] ChannelMessage channelMessage,
+            ILogger logger)
+        {
+            logger.LogInformation($"{channelMessage.Channel} => {channelMessage.Message}");
+            if(channelMessage.Message == "expired")
+            {
+                logger.LogInformation("EXPIRED"); 
+                // TODO: CALL API TO REFRESH CACHE HERE 
+            }
+        }
+    }
+}
