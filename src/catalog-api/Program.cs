3a902309e84ce34ab43058c0e4b6b47c872634a4
Damien Aicheh <damienaicheh@microsoft.com>
2023-09-01 08:55:28 +0200

    feat: Split product endpoints from Program.cs

--- src/catalog-api/Program.cs
+++ src/catalog-api/Program.cs
@@ -15,31 +15,6 @@ builder.Services.AddSingleton<IRedisService, RedisService>();
 var app = builder.Build();
 app.UseCors();
 
-app.MapGet("/products", async (ICosmosService cosmosService) => {
-    // Check the following links for queries: https://learn.microsoft.com/en-us/ef/core/providers/cosmos/?tabs=dotnet-core-cli#queries
-    var products = await cosmosService.RetrieveAllProductsAsync();
-    return Results.Ok(products);
-});
-
-app.MapGet("/products/{id}", async (ICosmosService cosmosService, IRedisService redisService, string id) => {
-    Product? cachedProduct = await redisService.GetProductAsync(id);
-
-    if (cachedProduct != null) {
-        Console.WriteLine("Returning a production description from the cache:");
-        Console.WriteLine(cachedProduct?.Id);
-
-        return Results.Ok(cachedProduct);
-    }
-
-    var product = await cosmosService.RetrieveProductByIdAsync(id);
-
-    if (product == null) {
-        return Results.NotFound();
-    }
-
-    await redisService.SetProductAsync(product);
-
-    return Results.Ok(product);
-});
+app.MapProductEndpoints();
 
 app.Run();
