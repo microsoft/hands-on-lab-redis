e2f9ee0947b6b03890adda5434b39350ca3a6fde
Iheb Khemissi <iheb.khemissi@gmail.com>
2023-09-26 22:25:58 +0200

    feat: Describe how to deploy the web app
    Describe how we can deploy the static web app which will be used by the learner to assess their progress in the lab.
    
    noissue

--- terraform/swa.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_static_site" "this" {
-  name                = format("stapp-%s", local.resource_suffix_kebabcase)
-  resource_group_name = azurerm_resource_group.this.name
-  location            = azurerm_resource_group.this.location
-  tags                = local.tags
-}
